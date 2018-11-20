23454f1ea9db31dd2fd6215d2a1a910f17c9de55
Cristian Boboc <cristi@pidginhost.com>
2018-11-20 16:20:01 +0200

    added success template

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
